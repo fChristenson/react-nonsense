10e3fe6e641174a76e624a2ce21412eb506768d1
Fredrik Christenson <fredrik.christenson@ticnet.se>
2016-03-03 09:40:45 +0100

    added scoreboard styles

--- app/public/javascript/apps/scoreboard/index.jsx
+++ app/public/javascript/apps/scoreboard/index.jsx
@@ -1,23 +1,22 @@
 'use strict';
 
 import React  from 'react';
-import Header from '../../components/header/index.jsx';
 import Avatar from '../../components/avatar/index.jsx';
 import Button from '../../components/button/index.jsx';
+import style  from './scoreboard.scss';
 
 export default (props) => {
   const rows = props.scores.map((score, index) => {
-    return <tr key={index}><td><Avatar player={score.player}/></td><td>{score.points}</td></tr>;  
+    return <tr key={index}><td className="scoreboard__table__cell"><Avatar player={score.player}/></td><td>{score.points}</td></tr>;
   });
   return (<div className="scoreboard">
-      <Header textTop={'Scoreboard'}>
+      <h1 className="scoreboard__header">Scoreboard</h1>
       <table className="scoreboard__table">
       <tbody>
       {rows} 
       </tbody>
       </table>
       <Button text={'Done'} onClick={props.endGame}/>
-      </Header>
       </div>);
 };
 
